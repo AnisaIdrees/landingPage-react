766d83ce170570fe88cc0bcc2ef7f12e45ef51f1
AnisaIdrees <AnisaIdrees191@gmail.com>
2025-03-25 21:39:57 +0500

    navbar creating...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class LandingPage extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <nav>
+        <h1>Coders World </h1>
+        <div class ="link-section">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Services</li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }
 
-export default App;
+
+export default LandingPage;
